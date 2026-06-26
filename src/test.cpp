e0390dd948c2169eab8e56053efa2c6e87aa498b
Harry Wu <310783731+xinghao-wu@users.noreply.github.com>
2026-06-26 15:42:31 -0700

    Add constants for testing
    Adds testing xdg dir global constants and minor formatting changes.

--- src/test.cpp
+++ src/test.cpp
@@ -4,13 +4,17 @@
 namespace fs = std::filesystem;
 
 namespace test {
-    extern const fs::path projectRoot {fs::current_path().parent_path()};
-    extern const fs::path mysteriesRoot {
-        projectRoot / "test_epubs/lord_of_mysteries_unzipped"};
-    extern const fs::path parasiteRoot {
-        projectRoot / "test_epubs/parasite_in_love_unzipped"};
-    extern const fs::path spiceWolfRoot {
-        projectRoot / "test_epubs/spice_and_wolf_vol_1_unzipped"};
-    extern const fs::path zuttomoRoot {
-        projectRoot / "test_epubs/zuttomo_vol_1_unzipped"};
+    // assumes executable is ran with the working directory being build/
+    const fs::path projectRoot {fs::current_path().parent_path()};
+
+    const fs::path epubs {projectRoot / "test_epubs"};
+    const fs::path mysteriesRoot {epubs / "lord_of_mysteries_unzipped"};
+    const fs::path parasiteRoot {epubs / "parasite_in_love_unzipped"};
+    const fs::path spiceWolfRoot {epubs / "spice_and_wolf_vol_1_unzipped"};
+    const fs::path zuttomoRoot {epubs / "zuttomo_vol_1_unzipped"};
+
+    const fs::path xdgDirs {projectRoot / "testing_xdg_dirs"};
+    const fs::path cache {xdgDirs / ".cache"};
+    const fs::path config {xdgDirs / ".config"};
+    const fs::path share {xdgDirs / "local/share"};
 }
--- src/test.hpp
+++ src/test.hpp
@@ -1,12 +1,2 @@
-#include <filesystem>
-
-namespace fs = std::filesystem;
-
 namespace test {
-    // assumes executable is ran with the working directory being build/
-    extern const fs::path projectRoot;
-    extern const fs::path mysteriesRoot;
-    extern const fs::path parasiteRoot;
-    extern const fs::path spiceWolfRoot;
-    extern const fs::path zuttomoRoot;
 }
