d7eaa213a6a71c939c299b75d5f0b2b37b9687c1
Harry Wu <310783731+xinghao-wu@users.noreply.github.com>
2026-06-26 00:24:52 -0700

    Update test epubs
    Removes Date This Super Cute Me and adds Lord of Mysteries as testing
    epubs, does some minor renaming of associated variables.

--- src/test.hpp
+++ src/test.hpp
@@ -5,9 +5,9 @@ namespace fs = std::filesystem;
 namespace test {
     // assumes executable is ran with the working directory being build/
     extern const fs::path projectRoot;
-    extern const fs::path superCuteMeRoot;
+    extern const fs::path mysteriesRoot;
     extern const fs::path parasiteRoot;
+    extern const fs::path spiceWolfRoot;
     extern const fs::path zuttomoRoot;
-    extern const fs::path spiceAndWolfRoot;
     extern const fs::path relContainerXML;
 }
