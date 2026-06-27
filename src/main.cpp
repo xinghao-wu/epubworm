5ca9293626b63b93a72ad7120d6245c96e7a91cd
Harry Wu <310783731+xinghao-wu@users.noreply.github.com>
2026-06-27 12:11:56 -0700

    Remove catch-all in `main()`

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,5 @@
-#include <exception>
-#include <iostream>
 #include "test.hpp"
 
 int main() {
-    try {
-    }
-    catch (const std::exception& e) {
-        std::cerr << "fatal error: " << e.what() << '\n';
-        return 1;
-    }
-
     return 0;
 }
