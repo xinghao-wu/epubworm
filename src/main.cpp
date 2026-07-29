cbc74e1e557553bd00bd346afc8dcf67d15e3b24
Harry Wu <310783731+xinghao-wu@users.noreply.github.com>
2026-07-29 12:16:09 -0700

    Add catch-all handler to `main()`

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,33 @@
+#include <exception>
+#include <stdexcept>
+#include <system_error>
+#include <iostream>
+
 int main() {
-    return 0;
+    try {
+        return 0;
+    }
+    catch (const std::system_error& e) {
+        std::cerr << "fatal system error occurred\n";
+        std::cerr << "error message: " << e.what() << '\n';
+        std::cerr << "error code: " << e.code().value() << "\n";
+        std::cerr << "error category: " << e.code().category().name() << "\n";
+        return 1;
+    }
+    catch (const std::runtime_error& e) {
+        std::cerr << "fatal runtime error occurred: " << e.what() << '\n';
+        return 1;
+    }
+    catch (const std::logic_error& e) {
+        std::cerr << "fatal logic error occurred: " << e.what() << '\n';
+        return 1;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "fatal standard exception occurred: " << e.what() << '\n';
+        return 1;
+    }
+    catch (...) {
+        std::cerr << "fatal non-standard exception occurred\n";
+        return 1;
+    }
 }
