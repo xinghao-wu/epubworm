0f170518b4a7d28af92c14f69f2efbff1f14ab77
Harry Wu <310783731+xinghao-wu@users.noreply.github.com>
2026-09-01 20:53:33 -0700

    Add newline in front of fatal exception msgs

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
         return dispatchCli(argc, argv);
     } catch (const std::system_error& e) {
         boldColorIfTerm(stderr, redFG);
-        std::cerr << "Fatal system error occurred\n";
+        std::cerr << "\nFatal system error occurred\n";
         std::cerr << "Error message: " << e.what() << '\n';
         std::cerr << "Error code: " << e.code().value() << "\n";
         std::cerr << "Error category: " << e.code().category().name() << "\n";
@@ -29,22 +29,23 @@ int main(int argc, char* argv[]) {
         return 1;
     } catch (const std::runtime_error& e) {
         boldColorIfTerm(stderr, redFG);
-        std::cerr << "Fatal runtime error occurred: " << e.what() << '\n';
+        std::cerr << "\nFatal runtime error occurred: " << e.what() << '\n';
         resetBoldColorIfTerm(stderr);
         return 1;
     } catch (const std::logic_error& e) {
         boldColorIfTerm(stderr, redFG);
-        std::cerr << "Fatal logic error occurred: " << e.what() << '\n';
+        std::cerr << "\nFatal logic error occurred: " << e.what() << '\n';
         resetBoldColorIfTerm(stderr);
         return 1;
     } catch (const std::exception& e) {
         boldColorIfTerm(stderr, redFG);
-        std::cerr << "Fatal standard exception occurred: " << e.what() << '\n';
+        std::cerr << "\nFatal standard exception occurred: " << e.what()
+                  << '\n';
         resetBoldColorIfTerm(stderr);
         return 1;
     } catch (...) {
         boldColorIfTerm(stderr, redFG);
-        std::cerr << "Fatal non-standard exception occurred\n";
+        std::cerr << "\nFatal non-standard exception occurred\n";
         resetBoldColorIfTerm(stderr);
         return 1;
     }
